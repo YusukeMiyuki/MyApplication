c4d5f941495f8b95ea75ca202d6fae251873c09f
akadamario <akadamax49@gmail.com>
2020-03-20 17:50:34 +0900

    作成中

---
+++ CSharp/ClassLib/ClassLib/ClassLib/EnvironmentInfo/EnvironmentInfo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ClassLib.EnvironmentInfo
+{
+    /// <summary>
+    /// 環境変数やマシーン、ログイン者などの環境全般を扱うクラス
+    /// </summary>
+    public static class EnvironmentInfo
+    {
+        #region CPUの最大スレッド数の取得
+        static int? mMaxThread;
+        /// <summary>
+        /// 最大スレッド数を取得する
+        /// </summary>
+        public static int MaxThread
+        {
+            get
+            {
+                if (mMaxThread.HasValue) return mMaxThread.Value;
+                ThreadPool.GetMinThreads(out var maxThread, out _);
+                mMaxThread = maxThread;
+                return mMaxThread.Value;
+            }
+        }
+        #endregion
+
+        #region 環境変数の値を取得する
+        /// <summary>
+        /// 指定した環境変数の値を取得する
+        /// </summary>
+        /// <param name="envVarName">環境変数名</param>
+        /// <param name="value">環境変数の値</param>
+        /// <returns>取得に成功したかどうか</returns>
+        public static bool GetEnvironmentVariable(string envVarName, out string value)
+        {
+            try
+            {
+                value = Environment.GetEnvironmentVariable(envVarName, EnvironmentVariableTarget.User);
+                if (value == null) value = Environment.GetEnvironmentVariable(envVarName, EnvironmentVariableTarget.Machine); // 権限がないと例外発生
+                if (value == null)
+                {
+                    value = string.Empty;
+                    return false;
+                }
+            }
+            catch 
+            { 
+                value = string.Empty;
+                return false;
+            }
+            return true;
+        }
+        #endregion
+    }
+}
---
+++ CSharp/GUI/ToDoTaskManageer/ToDoTaskManageer/DataSourceCS/ToDoTask.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoTaskManageer.DataSourceCS
+{
+    /// <summary>
+    /// ToDoタスク自身のクラス
+    /// </summary>
+    class ToDoTask
+    {
+        #region プロパティ
+        /// <summary>
+        /// タスク名
+        /// </summary>
+        public string TaskName { get; private set; }
+        /// <summary>
+        /// 優先度
+        /// </summary>
+        public int Priority { get; private set; }
+        /// <summary>
+        /// 期日
+        /// </summary>
+        public DateTime DeadLine { get; private set; }
+        /// <summary>
+        /// 実行が完了したか
+        /// </summary>
+        public bool IsDone { get; private set; }
+        /// <summary>
+        /// 担当者
+        /// </summary>
+        public string PersonInCharge { get; private set; }
+        #endregion
+
+        #region コンストラクタ
+        public ToDoTask(string taskName, int priority, DateTime deadLine, bool isDone = false, string person = null)
+        {
+            TaskName = taskName;
+            Priority = priority;
+            DeadLine = deadLine;
+            SetIsDone(isDone);
+            SetPersonInCharge(person);
+        }
+        #endregion
+
+        #region タスク名変更
+        /// <summary>
+        /// タスク名の変更
+        /// </summary>
+        /// <param name="taskName">タスク名</param>
+        /// <returns>変更できたかどうか</returns>
+        public bool ChangeTaskName(string taskName)
+        {
+            if (string.IsNullOrEmpty(taskName))
+            {
+
+                return false;
+            }
+            TaskName = taskName;
+            return true;
+        }
+        #endregion
+
+        #region 優先度の変更
+        /// <summary>
+        /// 優先度変更
+        /// </summary>
+        /// <param name="priority">優先度</param>
+        public void ChangePriority(int priority)
+        {
+            if (Priority == priority) return;
+            Priority = priority;
+        }
+        #endregion
+
+        #region 完了したかどうか
+        /// <summary>
+        /// 完了したかどうか
+        /// </summary>
+        /// <param name="isDone">true; 完了、false; 未完了</param>
+        public void SetIsDone(bool isDone) => IsDone = isDone;
+        #endregion
+
+        #region 担当者の変更
+        /// <summary>
+        /// 担当者の変更
+        /// </summary>
+        /// <param name="person">担当者名</param>
+        public void SetPersonInCharge(string person)
+        {
+            if (string.IsNullOrEmpty(person)) PersonInCharge = Environment.UserName; // 既定は実行ユーザ
+            else PersonInCharge = person;
+        }
+        #endregion
+    }
+}
